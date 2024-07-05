9d917226893cc23216fc2c9a900cdbfac8d04e43
Busola <busolaokemoney@gmail.com>
2024-07-05 16:06:55 +0100

    fix es

--- template/login-shortcode.php
+++ template/login-shortcode.php
@@ -1,7 +1,7 @@
 <?php
 
-$logo_id        	= get_theme_mod('custom_logo');
-$logo_image     	= wp_get_attachment_image_src($logo_id, 'full');
+$logo_id = get_theme_mod('custom_logo');
+$logo_image = wp_get_attachment_image_src($logo_id, 'full');
 
 //  register get link
 $page = skwp_get_page_by_title('Student Registration');
@@ -14,20 +14,25 @@ $page = skwp_get_page_by_title('Student Registration');
 				<img src="<?php echo esc_url($logo_image[0]); ?>" alt="<?php esc_html_e('logo', 'sakolawp'); ?>" />
 			</div>
 		<?php } ?>
-		<h4 class="sakolawp_header"><?php esc_html_e('Great to have back!', 'sakolawp'); ?></h4>
+		<h4 class="sakolawp_header"><?php esc_html_e('Great to have you back!', 'sakolawp'); ?></h4>
 		<?php sakolawp_show_error_messages(); ?>
 		<p>
-			<input name="sakolawp_user_login" id="sakolawp_user_login" class="required swkp-usr-form" type="text" placeholder="<?php esc_html_e('Username', 'sakolawp'); ?>" />
+			<input name="sakolawp_user_login" id="sakolawp_user_login" class="required swkp-usr-form" type="text"
+				placeholder="<?php esc_html_e('Username', 'sakolawp'); ?>" />
 		</p>
 		<p>
-			<input name="sakolawp_user_pass" id="sakolawp_user_pass" class="required swkp-usr-form" type="password" placeholder="<?php esc_html_e('Password', 'sakolawp'); ?>" />
+			<input name="sakolawp_user_pass" id="sakolawp_user_pass" class="required swkp-usr-form" type="password"
+				placeholder="<?php esc_html_e('Password', 'sakolawp'); ?>" />
 		</p>
 		<div class="login meta skwp-clearfix">
-			<p class="forgetmenot float-left"><label for="rememberme"><input name="rememberme" type="checkbox" id="rememberme" value="forever" /> <?php esc_html_e('Keep me logged in', 'sakolawp'); ?></label></p>
+			<p class="forgetmenot float-left"><label for="rememberme"><input name="rememberme" type="checkbox"
+						id="rememberme" value="forever" /> <?php esc_html_e('Keep me logged in', 'sakolawp'); ?></label>
+			</p>
 			<!-- <a href="<?php echo esc_url(get_permalink($page->ID)); ?>" class="register float-right"><?php esc_html_e('Not a member? Create an account', 'sakolawp'); ?></a> -->
 		</div>
 		<p>
-			<input id="skwp-login-btn" type="hidden" name="sakolawp_login_nonce" value="<?php echo wp_create_nonce('sakolawp-login-nonce'); ?>" />
+			<input id="skwp-login-btn" type="hidden" name="sakolawp_login_nonce"
+				value="<?php echo wp_create_nonce('sakolawp-login-nonce'); ?>" />
 			<input id="sakolawp_login_submit" type="submit" value="<?php echo esc_html__('Login', 'sakolawp'); ?>" />
 		</p>
 	</fieldset>
